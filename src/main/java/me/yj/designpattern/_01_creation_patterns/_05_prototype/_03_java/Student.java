a17f858b1ca15e9a0164758125d9232236cb1758
YoungJun Park <youngjun108059@gmail.com>
2021-12-15 00:04:42 +0900

    [Add] ✍️ 프로토타입패턴 - Java에 적용된 사례 추가

---
+++ src/main/java/me/yj/designpattern/_01_creation_patterns/_05_prototype/_03_java/Student.java
@@ -0,0 +1,17 @@
+package me.yj.designpattern._01_creation_patterns._05_prototype._03_java;
+
+public class Student {
+
+    String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
