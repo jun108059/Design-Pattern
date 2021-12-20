49b5d3088db56d5475bd3f10b39412f8f66dc37f
YoungJun Park <youngjun108059@gmail.com>
2021-12-20 23:01:27 +0900

    [Add] ✍️ 브릿지 패턴 - 적용 전 예제

---
+++ src/main/java/me/yj/designpattern/_02_structural_patterns/_07_bridge/_01_before/Champion.java
@@ -0,0 +1,17 @@
+package me.yj.designpattern._02_structural_patterns._07_bridge._01_before;
+
+import me.yj.designpattern._02_structural_patterns._07_bridge._02_after.Skin;
+
+public interface Champion extends Skin {
+
+    void move();
+
+    void skillQ();
+
+    void skillW();
+
+    void skillE();
+
+    void skillR();
+
+}
